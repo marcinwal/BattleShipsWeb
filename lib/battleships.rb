3188c5b7e4d8c15067b3a210b32cbc74402b0a90
marcin walendzik <marcin.walendzik@gmail.com>
2015-01-06 16:15:20 +0000

    working 3 pages

--- lib/battleships.rb
+++ lib/battleships.rb
@@ -5,6 +5,15 @@ class BattleShips < Sinatra::Base
     erb :index
   end
 
+  get '/newgame' do
+    erb :newgame
+  end
+
+  get '/maingame' do
+    @player1 = params[:name]
+    erb :maingame
+  end
+
   # start the server if ruby file executed directly
   run! if app_file == $0
 end
