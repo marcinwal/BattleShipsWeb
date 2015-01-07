6d69dc2e2e206bd6d5aed84f1cecdf5271e79be4
Richard Ighodaro <richard.ighodaro@hotmail.co.uk>
2015-01-07 14:26:52 +0000

    Added ship display logic

--- lib/battleships.rb
+++ lib/battleships.rb
@@ -1,5 +1,6 @@
 require 'sinatra/base'
 require 'byebug'
+require './lib/setup.rb'
 
 class BattleShips < Sinatra::Base
 
@@ -15,20 +16,39 @@ class BattleShips < Sinatra::Base
   end
 
   get '/maingame' do
-
     @player1 = params[:name]
+
+    @allowed_ships = Ship.methods(false)
+    @locations = Array[@allowed_ships.count]
+    @directions = dfbdfdfdf
 
     if session[:p1] == nil
       session[:p1] = @player1
+
+      p1 = Player.new
+      p1.name=session[:p1]
+      b1 = Board.new(Cell)
+      p1.board=b1
+
       erb :maingame
+
     elsif session[:p2] == nil
       session[:p2] = @player1
+
+      p1 = Player.new
+      p2.name=session[:p2]
+      b2 = Board.new(Cell)
+      p2.board=b2
+
       erb :maingame
+
     else
         erb :toolate
     end
-    @p1 = session[:p1]
-    @p2 = session[:p2]   
+    #
+    # @p1 = session[:p1]
+    # @p2 = session[:p2]
+
   end
 
   # start the server if ruby file executed directly
